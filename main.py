727498280ea0b53be055cf2257423cc24741bf29
Juan D Chaves <juandavidchaves@protonmail.com>
2024-05-01 17:06:21 -0300

    added scroll

--- main.py
+++ main.py
@@ -16,15 +16,14 @@ class Character():
         self.x = x
         self.y = y
         self.position = (x,y)
-        self.falling = True
         self.vertical_speed = 0
         self.jumped = False
         self.jumping = False
         self.move_right = False
         self.move_left = False
         self.shoot = False
+        self.scroll_limit = TILE_SIZE * 3
         self.scrolled = 0
-        self.scroll = False
 
         self.rect = pygame.Rect(x, y, CHARACTER_WIDTH, CHARACTER_HEIGHT)
 
@@ -36,25 +35,24 @@ class Character():
         delta_x = 0
         delta_y = 0
         obstacle_list = []
+        level_width = len(level_map[0]) * TILE_SIZE
 
         if self.move_left:
             delta_x -= SPEED
-
-            if self.rect.x + delta_x + self.scrolled <= TILE_SIZE * 3:
-                self.scrolled = 0
-                self.scroll = False
-                if self.rect.x + delta_x <= 0:
-                    delta_x = 0
+            
+            #Scroll Left
+            if self.rect.left + delta_x <= self.scroll_limit and self.scrolled != 0:
+                self.scrolled -= SPEED
 
         if self.move_right:
             delta_x += SPEED
-            if (self.rect.x + CHARACTER_WIDTH) + delta_x >= SCREEN_WIDTH:
-                delta_x = 0
             
-            #Scroll
-            if (self.rect.x + CHARACTER_WIDTH) + delta_x >= SCREEN_WIDTH - (TILE_SIZE * 3):
-                self.scrolled += SPEED
-                self.scroll = True
+            #Scroll Right
+            if self.rect.right + delta_x >= SCREEN_WIDTH - self.scroll_limit:
+                if self.scrolled >= level_width - SCREEN_WIDTH:
+                    self.scrolled = self.scrolled
+                else:
+                    self.scrolled += SPEED
         
         # A jump (Make sure it jumps only once each time)
         if self.jumped and self.jumping == False:
@@ -67,7 +65,7 @@ class Character():
         delta_y += self.vertical_speed
 
         for y, row in enumerate(range(12)):
-          for x, tile in enumerate(range(18)):
+          for x, tile in enumerate(range(24)):
                 if level_map[y][x] == "1":
                     obstacle_tile = pygame.Rect(((x * TILE_SIZE) - self.scrolled, y * TILE_SIZE),(CHARACTER_WIDTH, CHARACTER_HEIGHT))
                     obstacle_list.append(obstacle_tile)
@@ -80,13 +78,12 @@ class Character():
                     self.vertical_speed = 0
                     delta_y = obstacle.bottom - self.rect.top
                 elif self.vertical_speed >= 0:
-                    self.falling = False
                     self.jumping = False
                     self.vertical_speed = 0
                     delta_y = obstacle.top - self.rect.bottom
 
-            if self.rect.left + delta_x < 0 or self.rect.right + delta_x > SCREEN_WIDTH:
-                    delta_x = 0
+        if self.rect.left + delta_x < 0 or self.rect.right + delta_x > SCREEN_WIDTH:
+                delta_x = 0
                   
         self.rect.x += delta_x
         self.rect.y += delta_y
@@ -111,22 +108,23 @@ class Map:
 
     def map_1(self):
         level = [
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "1", "1", "1", "1", "1", "1", "1"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
                 ]
         return level
 
 t800 = Character(t800_x, t800_y)
+t1000 = Character(t800_x + 300, t800_y)
 map = Map()
 
 #######################
@@ -142,15 +140,16 @@ while running:
     # Draw map
     level_map = map.map_1()
     t800.update()
+    t1000.update()
     tile = None
     for y, row in enumerate(range(12)):
-        for x, tile in enumerate(range(18)):
+        for x, tile in enumerate(range(24)):
             if level_map[y][x] == "1":
                 tile = pygame.draw.rect(screen, "Gray", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
             else:
                 tile = pygame.draw.rect(screen, "Gray", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)),1)
 
-    position_text = f"x: {t800.rect.x} | y: {t800.rect.y} | v_speed: {t800.vertical_speed}"    
+    position_text = f"x: {t800.rect.x} | y: {t800.rect.y} | scrolled: {t800.scrolled}"    
     text = font.render(position_text, True, "white")
     screen.blit(text, (10,40))
 
