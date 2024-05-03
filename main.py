d74e23cbba42c3e3fd47d9069cab80718091a58e
Juan D Chaves <juandavidchaves@protonmail.com>
2024-05-03 16:33:53 -0300

    Map improved

--- main.py
+++ main.py
@@ -43,14 +43,17 @@ class Character():
             #Scroll Left
             if self.rect.left + delta_x <= self.scroll_limit and self.scrolled != 0:
                 self.scrolled -= SPEED
+                delta_x = 0
 
         if self.move_right:
             delta_x += SPEED
             
             #Scroll Right
             if self.rect.right + delta_x >= SCREEN_WIDTH - self.scroll_limit:
+                delta_x = 0
                 if self.scrolled >= level_width - SCREEN_WIDTH:
                     self.scrolled = self.scrolled
+                    delta_x += SPEED
                 else:
                     self.scrolled += SPEED
         
@@ -65,7 +68,7 @@ class Character():
         delta_y += self.vertical_speed
 
         for y, row in enumerate(range(12)):
-          for x, tile in enumerate(range(24)):
+          for x, tile in enumerate(range(48)):
                 if level_map[y][x] == "1":
                     obstacle_tile = pygame.Rect(((x * TILE_SIZE) - self.scrolled, y * TILE_SIZE),(CHARACTER_WIDTH, CHARACTER_HEIGHT))
                     obstacle_list.append(obstacle_tile)
@@ -92,6 +95,9 @@ class Character():
     def draw(self):
         pygame.draw.rect(screen, "red", ((self.rect.x, self.rect.y), (CHARACTER_WIDTH, CHARACTER_HEIGHT)))
 
+    def shoot(self):
+        pass
+
 #######################
 # MAP
 #######################
@@ -107,19 +113,20 @@ class Map:
         pass
 
     def map_1(self):
+        # 24 x 12
         level = [
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
-                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "5", "5", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "5", "5"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "5", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "5", "5", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "3", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "0", "0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "4", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "5", "5", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "1", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"],
+                    ["0", "0", "0", "0", "0", "0", "0", "1", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "8", "0"],
+                    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0", "0", "0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
+                    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
+                    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "2", "2", "2", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
                 ]
         return level
 
@@ -140,14 +147,39 @@ while running:
     # Draw map
     level_map = map.map_1()
     t800.update()
-    t1000.update()
+    # t1000.update()
     tile = None
     for y, row in enumerate(range(12)):
-        for x, tile in enumerate(range(24)):
+        for x, tile in enumerate(range(48)):
+            # 1 Block = saddlebrown
+            # 2 lava = organgered
+            # 3 Health = crimson
+            # 4 Ammo = orange
+            # 5 Deco1 = paleturquoise
+            # 6 Deco2 = papayawhip
+            # 7 Deco3 = palegreen
+            # 8 Exit = gold
+            # T800 = blue
+            # T1000 = black
             if level_map[y][x] == "1":
-                tile = pygame.draw.rect(screen, "Gray", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
-            else:
-                tile = pygame.draw.rect(screen, "Gray", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)),1)
+                tile = pygame.draw.rect(screen, "saddlebrown", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+            elif level_map[y][x] == "2":
+                tile = pygame.draw.rect(screen, "orangered", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+            elif level_map[y][x] == "3":
+                tile = pygame.draw.rect(screen, "crimson", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+            elif level_map[y][x] == "4":
+                tile = pygame.draw.rect(screen, "orange", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+            elif level_map[y][x] == "5":
+                tile = pygame.draw.rect(screen, "paleturquoise", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+            elif level_map[y][x] == "6":
+                tile = pygame.draw.rect(screen, "papayawhip", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+            elif level_map[y][x] == "7":
+                tile = pygame.draw.rect(screen, "palegreen", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+            elif level_map[y][x] == "8":
+                tile = pygame.draw.rect(screen, "gold", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)))
+                
+            # else:
+            #     tile = pygame.draw.rect(screen, "Gray", (((x * TILE_SIZE) - t800.scrolled, y * TILE_SIZE), (TILE_SIZE, TILE_SIZE)),1)
 
     position_text = f"x: {t800.rect.x} | y: {t800.rect.y} | scrolled: {t800.scrolled}"    
     text = font.render(position_text, True, "white")
